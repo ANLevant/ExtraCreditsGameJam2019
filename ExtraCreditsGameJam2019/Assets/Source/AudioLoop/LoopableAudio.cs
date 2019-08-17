516d9cabdc1c22cb3ce3df0de0fb58f1551a4d1e
ANLevant <levant.alejandro@hotmail.com>
2019-08-17 01:20:04 -0400

    Add loopableAudio c# class and audio to scene

---
+++ ExtraCreditsGameJam2019/Assets/Source/AudioLoop/LoopableAudio.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoopableAudio : MonoBehaviour {
+
+
+	AudioSource myAudio;
+	/**Perfect looping points for different songs:
+	* Smooth: 6 - 66 */
+	public float loopStart, loopEnd;
+	
+	// Use this for initialization
+	void Start(){
+		myAudio = GetComponent<AudioSource>();
+		myAudio.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (myAudio.isPlaying && myAudio.time >= loopEnd) {
+			myAudio.time = loopStart;
+		}		
+	}
+}
