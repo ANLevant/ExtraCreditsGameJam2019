054a7e7656f0a9dad064ba0a6483a959b07c5c25
ANLevant <levant.alejandro@hotmail.com>
2019-08-25 23:09:59 -0400

    Add ChallengeRoom logic and KillAllChallengeRoom

---
+++ ExtraCreditsGameJam2019/Assets/Source/ChallengeRoom/ChallengeRoom.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ChallengeRoom : MonoBehaviour {
+
+	// Use this for initialization
+	public void Start () {
+	}
+	
+	// Update is called once per frame
+	public void Update () {
+		if(FinishCondition()){
+		}
+	}
+
+	public abstract bool FinishCondition();
+}
---
+++ ExtraCreditsGameJam2019/Assets/Source/ChallengeRoom/KillThemAllChallengeRoomScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillThemAllChallengeRoomScript : ChallengeRoom {
+
+	// Use this for initialization
+	public new void Start () {
+		base.Start();
+	}
+	
+	// Update is called once per frame
+	public new void Update () {
+		base.Update();
+	}
+
+	public override bool FinishCondition(){
+		if(transform.childCount == 0){
+			Debug.Log("Finished!");
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+}
